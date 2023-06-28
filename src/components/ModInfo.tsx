a35d64cfefb47fd253b2c3440c64452c59ff4192
Pike <havelka.pvl@seznam.cz>
2023-06-28 20:21:35 +0200

    Oprava zobrazování módu na mobilech

--- src/components/ModInfo.tsx
+++ src/components/ModInfo.tsx
@@ -2,8 +2,8 @@ import "./ModInfo.scss"
 
 function ModInfo({ name, version, i } : { name: string, version: string, i: number }) {
   return <div className="one-mode">
-    <h3>{i + 1}. {name}</h3> 
-    <h3>{version}</h3>
+    <h3 className="name">{i + 1}. {name}</h3> 
+    <h3 className="version">{version}</h3>
   </div>
 }
 
